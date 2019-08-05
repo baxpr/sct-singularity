0250d7ea21bb2f4a872bc4a4ea555698b01e1bc7
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-08-05 12:20:59 -0500

    Update to mask before moco

--- scripts/fmri.sh
+++ scripts/fmri.sh
@@ -14,10 +14,17 @@ cp ../INPUTS/mffe_e1.nii.gz .
 MFFE=mffe_e1
 FMRI=fmri
 
-# Topup?
+# How big of a mask to use for registrations?
+MSIZE=30
+
+# Extract first fmri volume, find centerline, make mask
+sct_image -keep-vol 0 -i ${FMRI}.nii.gz -o ${FMRI}_0.nii.gz
+sct_get_centerline -c t2s -i ${FMRI}_0.nii.gz
+sct_create_mask -i ${FMRI}_0.nii.gz -p centerline,${FMRI}_0_centerline.nii.gz -size ${MSIZE}mm \
+-o ${FMRI}_mask${MSIZE}.nii.gz
 
 # fMRI motion correction
-sct_fmri_moco -i ${FMRI}.nii.gz 
+sct_fmri_moco -m ${FMRI}_mask${MSIZE}.nii.gz -i ${FMRI}.nii.gz 
 
 # Find cord on mean fMRI to improve registration
 sct_deepseg_sc -i ${FMRI}_moco_mean.nii.gz -c t2s
@@ -26,16 +33,25 @@ sct_deepseg_sc -i ${FMRI}_moco_mean.nii.gz -c t2s
 sct_deepseg_sc -i ${MFFE}.nii.gz -c t2
 
 # Create mask for registration
-sct_create_mask -i ${MFFE}.nii.gz -p centerline,${MFFE}_seg.nii.gz -size 30mm \
--o ${MFFE}_mask30.nii.gz
+sct_create_mask -i ${MFFE}.nii.gz -p centerline,${MFFE}_seg.nii.gz -size ${MSIZE}mm \
+-o ${MFFE}_mask${MSIZE}.nii.gz
 
 # Register mean fMRI to mFFE
 sct_register_multimodal \
 -i ${FMRI}_moco_mean.nii.gz -iseg ${FMRI}_moco_mean_seg.nii.gz \
 -d ${MFFE}.nii.gz -dseg ${MFFE}_seg.nii.gz \
--m ${MFFE}_mask30.nii.gz \
+-m ${MFFE}_mask${MSIZE}.nii.gz \
 -param step=1,type=seg,algo=centermass,metric=MeanSquares,smooth=2:\
 step=2,type=im,algo=slicereg,metric=MI
+
+
+# RETROICOR
+# First split physlog into card and resp, and trim to match length of scan.
+# python is /opt/sct/python/envs/venv_sct/bin/python
+# Get scan length with nibabel (sct python has it) and fmri_moco.nii.gz?
+#    img.header.dim[4] and img.header.pixdim[4]
+#    pixdim[4] for 3D fmri is per shot not per volume
+3dretroicor -prefix pfx -card crd -resp resp -order 2 -cardphase cphase -respphase rphase ${FMRI}_moco.nii.gz
 
 # Next:
 #

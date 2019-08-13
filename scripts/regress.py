c3c255f3c6e7ca76cc0cb87d041f9dd7c404bdb9
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-08-13 15:32:34 -0500

    Regression confound removal done

--- scripts/regress.py
+++ scripts/regress.py
@@ -10,6 +10,8 @@ fmri_file = 'fmri_moco.nii.gz'
 csf_file = 'fmri_moco_CSF.nii.gz'
 notspine_file = 'fmri_moco_NOTSPINE.nii.gz'
 
+numPCs = 5
+
 # Cardiac/respiratory. We apply the same ones to all slices, assuming
 # 3D fmri acquisition sequence. ricor_file is the appropriate output
 # from RetroTS.py
@@ -20,79 +22,96 @@ ricor_data /= numpy.std(ricor_data,0)
 
 # fmri time series data
 fmri_img = nibabel.load(fmri_file)
-nvols = fmri_img.header.get_data_shape()[3]
+dims = fmri_img.header.get_data_shape()
+nslices = dims[2]
+nvols = dims[3]
 
 # CSF and NOTSPINE mask images in fmri space
 csf_img = nibabel.load(csf_file)
 notspine_img = nibabel.load(notspine_file)
 
 # Verify that all images have the same geometry
-#if not ( (csf_img.affine==notspine_img.affine).all() and
-#         (csf_img.affine==fmri_img.affine).all() ):
-#    raise Exception('affine mismatch in image files')
+if not ( (csf_img.affine==notspine_img.affine).all() and
+         (csf_img.affine==fmri_img.affine).all() ):
+    raise Exception('affine mismatch in image files')
 
 # Check that slice axis is third and get number of slices
-dims = csf_img.header.get_data_shape()
-#if not (dims[2]<dims[0] and dims[2]<dims[1]):
-#    raise Exception('Third dimension is not slice dimension?')
-nslices = dims[2]
+if not (dims[2]<dims[0] and dims[2]<dims[1]):
+    raise Exception('Third dimension is not slice dimension?')
+print('Found %d slices, %d vols' % (nslices,nvols))
 
 # Get fmri data and reshape to inslice x thruslice x time. Reslice
-# appears to copy
+# appears to copy. Make empty for filtered data
+print('Loading fmri')
 fmri_data = fmri_img.get_data();
 rfmri_data = numpy.reshape(fmri_data,(dims[0]*dims[1],nslices,nvols),order='F')
+frfmri_data = numpy.zeros(rfmri_data.shape)
 
 # Binarize and reshape CSF and NOTSPINE masks
+print('Confound masks')
 csf_mask = numpy.greater(csf_img.get_data(),0)
 rcsf_mask = numpy.reshape(csf_mask,(dims[0]*dims[1],nslices),order='F')
 ns_mask = numpy.greater(notspine_img.get_data(),0)
 rns_mask = numpy.reshape(ns_mask,(dims[0]*dims[1],nslices),order='F')
 
+#
+print('Slicewise correction')
+for s in range(nslices):
 
-s = 0
+    # This slice fmri data
+    sfmri_data = rfmri_data[:,s,:].T
 
-# This slice fmri data
-sfmri_data = rfmri_data[:,s,:].T
+    # Noise data, time x voxel
+    csf_data = numpy.copy(rfmri_data[rcsf_mask[:,s],s,:]).T
+    ns_data = numpy.copy(rfmri_data[rns_mask[:,s],s,:]).T
 
-# Noise data, time x voxel
-csf_data = numpy.copy(rfmri_data[rcsf_mask[:,s],s,:]).T
-ns_data = numpy.copy(rfmri_data[rns_mask[:,s],s,:]).T
+    # Normalize - subtract time mean, time sd = 1. Drop constant-valued voxels
+    numpy.seterr(invalid='ignore')
+    csf_data -= numpy.mean(csf_data,0)
+    csf_data /= numpy.std(csf_data,0)
+    csf_data = csf_data[:,numpy.logical_not(numpy.isnan(numpy.std(csf_data,0)))]
+    ns_data -= numpy.mean(ns_data,0)
+    ns_data /= numpy.std(ns_data,0)
+    ns_data = ns_data[:,numpy.logical_not(numpy.isnan(numpy.std(ns_data,0)))]
+    numpy.seterr(invalid='warn')
 
-# Normalize - subtract time mean, time sd = 1. Drop constant-valued voxels
-csf_data -= numpy.mean(csf_data,0)
-csf_data /= numpy.std(csf_data,0)
-csf_data = csf_data[:,numpy.logical_not(numpy.isnan(numpy.std(csf_data,0)))]
-ns_data -= numpy.mean(ns_data,0)
-ns_data /= numpy.std(ns_data,0)
-ns_data = ns_data[:,numpy.logical_not(numpy.isnan(numpy.std(ns_data,0)))]
+    # Get largest eigenvalue components and pct variance explained
+    csf_PCs,csf_S,V = numpy.linalg.svd(csf_data, full_matrices=False)
+    csf_var = numpy.square(csf_S)
+    csf_var = csf_var / sum(csf_var)
+    ns_PCs,ns_S,V = numpy.linalg.svd(ns_data, full_matrices=False)
+    ns_var = numpy.square(ns_S)
+    ns_var = ns_var / sum(ns_var)
 
-# Get largest eigenvalue components and pct variance explained
-csf_PCs,csf_S,V = numpy.linalg.svd(csf_data, full_matrices=False)
-csf_var = numpy.square(csf_S)
-csf_var = csf_var / sum(csf_var)
-ns_PCs,ns_S,V = numpy.linalg.svd(ns_data, full_matrices=False)
-ns_var = numpy.square(ns_S)
-ns_var = ns_var / sum(ns_var)
+    # Combine and rescale the desired confound regressors
+    confounds = numpy.hstack((ricor_data,csf_PCs[:,0:numPCs],ns_PCs[:,0:numPCs]))
+    confounds -= numpy.mean(confounds,0)
+    confounds /= numpy.std(confounds,0)
+    confounds1 = numpy.hstack((confounds,numpy.ones((nvols,1))))
+    #numpy.savetxt('confounds_slice%03d.csv' % s,confounds,delimiter=',')
 
-numPCs = 5
-numpy.savetxt('ricor.txt',ricor_data)
-numpy.savetxt('csf.txt',csf_PCs[:,0:numPCs])
-numpy.savetxt('ns.txt',ns_PCs[:,0:numPCs])
+    # Remove confounds from this slice (except the mean) and store
+    #  doc notation: b = ax,   x = numpy.linalg.lstsq(a,b)
+    #   my notation: y = xb,   b = numpy.linalg.lstsq(x,y)
+    beta1,sumresid,rank,svals = numpy.linalg.lstsq(confounds1,sfmri_data,rcond=None)
+    residual = sfmri_data - numpy.matmul(confounds,beta1[0:-1,:])
+    frfmri_data[:,s,:] = residual.T
 
-# Combine and rescale the desired confound regressors
-confounds = numpy.hstack((ricor_data,csf_PCs[:,0:numPCs],ns_PCs[:,0:numPCs]))
-confounds -= numpy.mean(confounds,0)
-confounds /= numpy.std(confounds,0)
-confounds = numpy.hstack((confounds,numpy.ones((nvols,1))))
-numpy.savetxt('confounds.csv',confounds,delimiter=',')
+print(frfmri_data.shape)
 
-# Remove confounds from this slice
-print(confounds.shape)
-print(sfmri_data.shape)
-beta,resid,rank,svals = numpy.linalg.lstsq(confounds,sfmri_data)
+# Reshape filtered fmri and save
+ffmri_data = numpy.reshape(frfmri_data,dims,order='F')
+ffmri_img = nibabel.Nifti1Image(ffmri_data,fmri_img.affine,fmri_img.header)
+nibabel.save(ffmri_img,'f'+fmri_file)
+
+# Test re-save of re-reshaped original data to verify correct reshaping
+#test_data = numpy.reshape(rfmri_data,dims,order='F')
+#test_img = nibabel.Nifti1Image(test_data,fmri_img.affine,fmri_img.header)
+#nibabel.save(test_img,'test.nii.gz')
+
+# Store all confound predictors in a .nii slicewise
 
 
-
-import matplotlib.pyplot
-matplotlib.pyplot.plot(csf_PCs[:,0:5])
-matplotlib.pyplot.show()
+#import matplotlib.pyplot
+#matplotlib.pyplot.plot(csf_PCs[:,0:5])
+#matplotlib.pyplot.show()

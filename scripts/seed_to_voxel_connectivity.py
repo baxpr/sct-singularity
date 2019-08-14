700cd9bef3daefdfadf2484e98a8d93b0e8ecfbd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-08-14 15:17:51 -0500

    Switching to nilearn for connectivity

--- scripts/seed_to_voxel_connectivity.py
+++ scripts/seed_to_voxel_connectivity.py
@@ -8,7 +8,7 @@ from nilearn.signal import high_variance_confounds
 
 ricor_file = 'ricor.slibase.1D'
 csf_file = 'fmri_moco_CSF.nii.gz'
-notspine_file = 'fmri_moco_NOTSPINE.nii.gz'
+ns_file = 'fmri_moco_NOTSPINE.nii.gz'
 
 fmri_file = 'ffmri_moco.nii.gz'
 seed_file = 'fmri_moco_GMcutlabel.nii.gz'
@@ -23,11 +23,20 @@ with open(vat_file,'r') as f:
 ricor_data = numpy.genfromtxt(ricor_file,skip_header=5,skip_footer=0)
 print('Found phys data size %d,%d' % ricor_data.shape)
 
-# CSF and not-spine signals (whole brain)
+# CSF and not-spine signals (entire 3D FOV)
 csf_masker = NiftiMasker(csf_file)
-csf_data = csf_masker.fit_transform(fmri_file)
-csf_confound_data = high_variance_confounds(csf_data,percentile=50)
-print('CSF data size %d,%d' % csf_confound_data.shape)
+csf_data = csf_masker.fit_transform(fmri_file,detrend=True)
+csf_confounds_data = high_variance_confounds(csf_data,percentile=50)
+print('CSF data size %d,%d' % csf_confounds_data.shape)
+
+ns_masker = NiftiMasker(ns_file)
+ns_data = csf_masker.fit_transform(fmri_file,detrend=True)
+ns_confounds_data = high_variance_confounds(ns_data,percentile=50)
+print('NOTSPINE data size %d,%d' % ns_confounds_data.shape)
+
+# Combine confound time series
+confounds_data = numpy.hstack((ricor_data,csf_confounds_data,ns_confounds_data))
+print('Confounds data size %d,%d' % confounds_data.shape)
 
 # ROI seed time series, filtered
 seed_masker = NiftiLabelsMasker(seed_file,standardize=True,detrend=True,

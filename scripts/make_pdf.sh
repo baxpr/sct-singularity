b54fe28ec2b068aaa2fc7e0a32683434aa99303d
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-08-23 12:43:27 -0500

    PDF with overlay

--- scripts/make_pdf.sh
+++ scripts/make_pdf.sh
@@ -10,6 +10,11 @@ FSLEYES=/opt/sct/python/envs/venv_sct/bin/fsleyes
 TDIR=/opt/sct/data/PAM50/template/
 
 ${FSLEYES} render \
---scene lightbox \
---outfile test.png --size 800 800 \
-"${TDIR}"/PAM50_t2s.nii.gz
+--scene ortho \
+--xzoom 1000 --yzoom 1000 --zzoom 2500 \
+--outfile test.png --size 1800 600 \
+mffe1.nii.gz \
+connectivity_r_slice_mffespace.nii.gz \
+--useNegativeCmap \
+--cmap red-yellow --negativeCmap blue-lightblue \
+--displayRange 0.5 1.0

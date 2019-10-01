62d104b1363dd75739b63e5e494c9ff8c3538fbd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-10-01 17:50:10 -0500

    add test script

---
+++ scripts/testrun.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH=/wkdir/scripts:${PATH}
+
+fmri_pipeline_launch.sh \
+--outdir ../OUTPUTS \
+--mffe_dir ../INPUTS/mffe_dir \
+--fmri_niigz ../INPUTS/fmri.nii.gz \
+--fmri_dcm ../INPUTS/fmri.dcm \
+--physlog_dcm ../INPUTS/physlog.dcm \
+--initcenter 3 \
+--masksize 30 \
+--project TESTPROJ \
+--subject TESTSUBJ \
+--session TESTSESS \
+--scan TESTSCAN

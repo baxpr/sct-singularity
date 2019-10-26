b2192f563b9c0673d9ec5e81aae584c941e8d617
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-10-25 19:07:40 -0500

    Working on moco

--- scripts/fmri_pipeline.sh
+++ scripts/fmri_pipeline.sh
@@ -15,6 +15,7 @@ TDIR=${SCTDIR}/data/PAM50/template
 # Use first echo of mffe
 cp mffe1.nii.gz mffe_mffe.nii.gz
 cp t2sag.nii.gz t2sag_t2sag.nii.gz
+cp fmri.nii.gz fmri_fmri.nii.gz
 
 # Segment GM and WM on mffe
 sct_deepseg_sc -i mffe_mffe.nii.gz -c t2
@@ -108,45 +109,61 @@ sct_apply_transfo -i t2sag_cord_labeled.nii.gz -d PAM50_synt2.nii.gz \
 	-x nn -o PAM50_cord_labeled.nii.gz
 
 
-exit 0
-
-
 # Extract first fmri volume, find centerline, make fmri space mask
-sct_image -keep-vol 0 -i ${FMRI}.nii.gz -o ${FMRI}_0.nii.gz
-sct_get_centerline -c t2s -i ${FMRI}_0.nii.gz
-sct_create_mask -i ${FMRI}_0.nii.gz -p centerline,${FMRI}_0_centerline.nii.gz -size ${MASKSIZE}mm \
--o ${FMRI}_mask${MASKSIZE}.nii.gz
+sct_image -keep-vol 0 -i fmri_fmri.nii.gz -o fmri_fmri0.nii.gz
+sct_get_centerline -c t2s -i fmri_fmri0.nii.gz
+mv fmri_fmri0_centerline.nii.gz fmri_centerline.nii.gz
+mv fmri_fmri0_centerline.csv fmri_centerline.csv
+sct_create_mask -i fmri_fmri0.nii.gz -p centerline,fmri_centerline.nii.gz -size ${MASKSIZE}mm \
+-o fmri_mask${MASKSIZE}.nii.gz
 
 # fMRI motion correction
-sct_fmri_moco -m ${FMRI}_mask${MASKSIZE}.nii.gz -i ${FMRI}.nii.gz 
+sct_fmri_moco -m fmri_mask${MASKSIZE}.nii.gz -i fmri_fmri.nii.gz 
+mv fmri_fmri_moco.nii.gz fmri_moco.nii.gz
+mv fmri_fmri_moco_mean.nii.gz fmri_moco_mean.nii.gz
+mv fmri_fmri_moco_params_X.nii.gz fmri_moco_params_X.nii.gz
+mv fmri_fmri_moco_params_Y.nii.gz fmri_moco_params_Y.nii.gz
+mv fmri_fmri_moco_params.tsv fmri_moco_params.tsv
+rm fmri_fmri_T????.nii.gz
+
 
 # Find cord on mean fMRI to improve registration
-sct_deepseg_sc -i ${FMRI}_moco_mean.nii.gz -c t2s
+sct_deepseg_sc -i fmri_moco_mean.nii.gz -c t2s
+mv fmri_moco_mean_seg.nii.gz fmri_cord.nii.gz
 
 # Register mean fMRI to mFFE
 sct_register_multimodal \
--i ${FMRI}_moco_mean.nii.gz -iseg ${FMRI}_moco_mean_seg.nii.gz \
--d ${MFFE}.nii.gz -dseg ${MFFE}_seg.nii.gz \
--m ${MFFE}_mask${MASKSIZE}.nii.gz \
+-i fmri_moco_mean.nii.gz -iseg fmri_cord.nii.gz \
+-d mffe_mffe.nii.gz -dseg mffe_cord.nii.gz \
+-m mffe_mask${MASKSIZE}.nii.gz \
 -param step=1,type=seg,algo=centermass,metric=MeanSquares,smooth=2:\
 step=2,type=im,algo=slicereg,metric=MI
 
+mv warp_fmri_moco_mean2mffe_mffe.nii.gz warp_fmri2mffe.nii.gz
+mv warp_mffe_mffe2fmri_moco_mean.nii.gz warp_mffe2fmri.nii.gz
+mv fmri_moco_mean_reg.nii.gz mffe_moco_mean.nii.gz
+mv mffe_mffe_reg.nii.gz fmri_mffe.nii.gz
+
+
+exit 0
+
+
 # Warp template t2s to mffe space
 sct_apply_transfo -i ${TDIR}/PAM50_t2s.nii.gz \
--w warp_PAM50_gw2i${MFFE}_gw.nii.gz \
--d ${MFFE}_gw.nii.gz -o PAM50_t2s_mffespace.nii.gz
+-w warp_PAM502mffe.nii.gz \
+-d mffe_mffe.nii.gz -o PAM50_t2s_mffespace.nii.gz
 
 # Warp mask to template space and trim template space images to actual FOV
-sct_apply_transfo -i ${MFFE}_mask${MASKSIZE}.nii.gz -x nn \
--w warp_i${MFFE}_gw2PAM50_gw.nii.gz \
--d ${TDIR}/PAM50_t2s.nii.gz  -o ${MFFE}_mask${MASKSIZE}_PAM50space.nii.gz
+sct_apply_transfo -i mffe_mask${MASKSIZE}.nii.gz -x nn \
+-w warp_mffe2PAM50.nii.gz \
+-d ${TDIR}/PAM50_t2s.nii.gz  -o PAM50_mask${MASKSIZE}.nii.gz
 
 sct_crop_image -i ${TDIR}/PAM50_t2s.nii.gz \
--m ${MFFE}_mask${MASKSIZE}_PAM50space.nii.gz \
+-m PAM50_mask${MASKSIZE}.nii.gz \
 -o PAM50_t2s_cropped.nii.gz
 
 # Warp mean fmri, mffe, gm, ROIs to template space
-sct_apply_transfo -i ${FMRI}_moco_mean.nii.gz \
+sct_apply_transfo -i fmri_moco_mean.nii.gz \
 -w warp_${FMRI}_moco_mean2mffe1.nii.gz warp_i${MFFE}_gw2PAM50_gw.nii.gz \
 -d PAM50_t2s_cropped.nii.gz  -o ${FMRI}_moco_mean_PAM50space.nii.gz
 
